111ac97d3ebc0051ce1aff500d15c6d68291efa5
Aliyu Abubakar <aliyufari@gmail.com>
2022-06-12 19:43:18 +0100

    Core class created

---
+++ app/controllers/Home.php
@@ -0,0 +1,16 @@
+<?php 
+	class Home{
+		
+		public function __construct()
+		{
+			
+		}
+
+		public function index(){
+			echo "Index Method";
+		}
+
+		public function about(){
+			echo "About Method";
+		}
+	}
--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -5,14 +5,39 @@
 		private $params = [];
 
 		public function __construct(){
-			echo "<pre>";
-			var_dump($_SERVER);
+			$url = $this->getUrl();
+
+			if (file_exists('..controllers/'. ucwords($url[0]) . '.php')) {
+
+				$this->currentController = ucwords($url[0]);
+				unset($url[0]);
+			}
+
+			require_once '../app/controllers/'. $this->currentController .'.php';
+			$this->currentController = new $this->currentController;
+
+			if (isset($url[1])) {
+				if (method_exists($this->currentController, $url[1])) {
+					$this->currentMethod = $url[1];
+					unset($url[1]);
+				}
+			}
+
+			$this->params = $url ? array_values($url) : [];
+			
+			call_user_func_array([$this->currentController, $this->currentMethod], $this->params);
 		}
 
 		private function getUrl(){
-			var_dump($_SERVER);
-			// if (isset($_SERVER['REQUEST_URL'])) {
-			// 	var_dump($_SERVER['REQUEST_URL']);
-			// }
+			if (isset($_GET['url'])) {
+
+				$url = trim($_GET['url'], '/');
+
+				$url = filter_var($url, FILTER_SANITIZE_URL);
+
+				$url = explode('/', $url);
+
+				return $url;
+			}
 		}
 	}
